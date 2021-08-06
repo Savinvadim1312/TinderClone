b58d5a3de1b435df1821044e8e60e0f6eb0bafc0
Vadim Savin <savinvadim1312@gmail.com>
2021-08-06 15:50:05 +0100

    icons and navigation

--- App.js
+++ App.js
@@ -1,23 +1,67 @@
 import 'react-native-gesture-handler';
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Pressable, SafeAreaView} from 'react-native';
+
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 import HomeScreen from './src/screens/HomeScreen';
 import MatchesScreen from './src/screens/MatchesScreen';
 
 const App = () => {
+  const [activeScreen, setActiveScreen] = useState('HOME');
+
+  const color = '#b5b5b5';
+  const activeColor = '#F76C6B';
   return (
-    <View style={styles.pageContainer}>
-      <HomeScreen />
-    </View>
+    <SafeAreaView style={styles.root}>
+      <View style={styles.pageContainer}>
+        <View style={styles.topNavigation}>
+          <Pressable onPress={() => setActiveScreen('HOME')}>
+            <Fontisto
+              name="tinder"
+              size={30}
+              color={activeScreen === 'HOME' ? activeColor : color}
+            />
+          </Pressable>
+          <MaterialCommunityIcons
+            name="star-four-points"
+            size={30}
+            color={color}
+          />
+          <Pressable onPress={() => setActiveScreen('CHAT')}>
+            <Ionicons
+              name="ios-chatbubbles"
+              size={30}
+              color={activeScreen === 'CHAT' ? activeColor : color}
+            />
+          </Pressable>
+          <FontAwesome name="user" size={30} color={color} />
+        </View>
+
+        {activeScreen === 'HOME' && <HomeScreen />}
+        {activeScreen === 'CHAT' && <MatchesScreen />}
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
   pageContainer: {
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
+  },
+  topNavigation: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 10,
   },
 });
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Entypo from 'react-native-vector-icons/Entypo';
+
 import Card from '../components/TinderCard';
 import users from '../../assets/data/users';
 
@@ -22,6 +27,27 @@ const HomeScreen = () => {
         onSwipeLeft={onSwipeLeft}
         onSwipeRight={onSwipeRight}
       />
+      <View style={styles.icons}>
+        <View style={styles.button}>
+          <FontAwesome name="undo" size={30} color="#FBD88B" />
+        </View>
+
+        <View style={styles.button}>
+          <Entypo name="cross" size={30} color="#F76C6B" />
+        </View>
+
+        <View style={styles.button}>
+          <FontAwesome name="star" size={30} color="#3AB4CC" />
+        </View>
+
+        <View style={styles.button}>
+          <FontAwesome name="heart" size={30} color="#4FCC94" />
+        </View>
+
+        <View style={styles.button}>
+          <Ionicons name="flash" size={30} color="#A65CD2" />
+        </View>
+      </View>
     </View>
   );
 };
@@ -32,6 +58,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
     width: '100%',
+    backgroundColor: '#ededed',
+  },
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 10,
+  },
+  button: {
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 50,
   },
 });
 
